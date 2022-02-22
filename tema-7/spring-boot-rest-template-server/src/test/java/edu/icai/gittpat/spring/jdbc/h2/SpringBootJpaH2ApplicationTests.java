662b890f1a17186fefe7463c329d8fe0eacb0891
osanzs <oscar.sanz@gmail.com>
2022-02-22 20:39:26 +0100

    Server Example + SpringFox

---
+++ tema-7/spring-boot-rest-template-server/src/test/java/edu/icai/gittpat/spring/jdbc/h2/SpringBootJpaH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.icai.gittpat.spring.jdbc.h2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJdbcH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

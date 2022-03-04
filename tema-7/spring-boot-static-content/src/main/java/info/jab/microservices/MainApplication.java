21a076609fc08f057c3bea5147d0bfb0635d1c7e
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-03-04 18:46:13 +0100

    Añadiendo ejemplo de Logging

--- tema-7/spring-boot-static-content/src/main/java/info/jab/microservices/MainApplication.java
+++ tema-7/spring-boot-static-content/src/main/java/info/jab/microservices/MainApplication.java
@@ -1,12 +1,17 @@
 package info.jab.microservices;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MainApplication {
 
+    public static Logger logger = LoggerFactory.getLogger(MainApplication.class);
+
     public static void main(String[] args) {
+        logger.info("Estoy usando Logging");
         SpringApplication.run(MainApplication.class, args);
     }
 
